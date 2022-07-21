6d89179a51dd23094c6d50a79fea80496e40152d
huangyisan <anonymousyisan@gmail.com>
2022-07-21 19:26:17 +0800

    add vuetify

--- vuetify-app/src/views/AboutView.vue
+++ vuetify-app/src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>This is an about page</h1>
+    <div class="pa-6">
+      <p>aaaaaaa</p>
+      <p>aaaaaaa</p>
+      <p>aaaaaaa</p>
+    </div>
   </div>
 </template>
