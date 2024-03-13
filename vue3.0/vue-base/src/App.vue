226294f37ee74a5188620f15cba52a0347be1b3a
Yisan Huang <anonymousyisan@gmail.com>
2024-03-13 10:24:25 +0800

    feat: update

---
+++ vue3.0/vue-base/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+let isButtonDisabled = ref(true)
+const objectOfAttrs = {
+  id: "container",
+  class: "wrapper"
+}
+let message = ref("abcdefg")
+</script>
+
+<template>
+    <button :disabled="isButtonDisabled" v-bind="objectOfAttrs">button</button>
+
+  <p>
+    {{ message.split('').reverse().join('') }}
+  </p>
+  
+</template>
+
+<style scoped>
+</style>
