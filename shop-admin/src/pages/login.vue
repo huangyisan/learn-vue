8909d568f970a285c48bc423a949afc3ed0ac359
Yisan Huang <anonymousyisan@gmail.com>
2024-03-29 18:02:46 +0800

    feat: update

--- shop-admin/src/pages/login.vue
+++ shop-admin/src/pages/login.vue
@@ -19,12 +19,12 @@
                 <span class="h-[1px] w-16 bg-gray-200"></span>
             </div>
             <div>
-                <el-form :model="form" class="w-[250px]">
-                    <el-form-item>
+                <el-form ref="formRef" :rules="rules" :model="form" class="w-[250px]">
+                    <el-form-item prop="username">
                         <el-input v-model="form.username" placeholder="请输入用户名" />
                     </el-form-item>
-                    <el-form-item>
-                        <el-input v-model="form.password" placeholder="请输入密码" />
+                    <el-form-item prop="password">
+                        <el-input v-model="form.password" placeholder="请输入密码" show-password type="password" />
                     </el-form-item>
 
                     <el-form-item>
@@ -38,15 +38,51 @@
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref, } from 'vue'
+import { login } from '~/api/manager'
+import { ElNotification } from 'element-plus'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
 // do not use same name with ref
 const form = reactive({
     username: "",
     password: "",
 })
 
-const onSubmit = () => {
-    console.log('submit!')
+const rules = {
+    username: [{ required: true, message: 'Please input Activity name', trigger: 'blur' },
+    { min: 3, max: 20, message: 'Length should be 3 to 20', trigger: 'blur' }],
+    password: [{ required: true, message: 'Please input password', trigger: 'blur' },
+    { min: 3, max: 20, message: 'Length should be 3 to 20', trigger: 'blur' }]
 }
+
+const formRef = ref(null)
+
+const onSubmit = () => {
+    formRef.value.validate((valid) => {
+        if (!valid) {
+            return false
+        }
+        login(form.username, form.password).then(res => {
+            console.log(res.data.data);
+            // 通知登陆成功
+            ElNotification({
+                message: "登陆成功",
+                type: "success",
+                duration: 3000,
+            })
+            // 跳转到首页
+            router.push("/")
+
+        }).catch(err => {
+            ElNotification({
+                message: err.response.data.msg || "请求失败",
+                type: "error",
+                duration: 3000,
+            })
+        })
+    })
+}
+
 </script>
